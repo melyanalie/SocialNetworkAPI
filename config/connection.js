4ef202097ac813a24dded7f6f16d5a1485b5d087
melyanalie <melyana.lie86@gmail.com>
2024-11-05 10:46:28 -0500

    updated connections.js

--- config/connection.js
+++ config/connection.js
@@ -1,8 +1,8 @@
 const mongoose = require('mongoose');
+require('dotenv').config();
 
-mongoose.connect('mongodb://127.0.0.1:27017/socialNetworkDB', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
+const connectionString = process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/socialnetworkapi';
+
+mongoose.connect(connectionString);
 
 module.exports = mongoose.connection;
