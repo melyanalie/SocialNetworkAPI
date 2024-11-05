6572c461b821a4cf10fad1cfe73bf73a6062b00b
melyanalie <melyana.lie86@gmail.com>
2024-11-05 11:25:54 -0500

    fixed connection.js

--- config/connection.js
+++ config/connection.js
@@ -1,8 +1,14 @@
 const mongoose = require('mongoose');
+
+mongoose.set('strictQuery', false);
+
 require('dotenv').config();
 
-const connectionString = process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/socialnetworkapi';
-
-mongoose.connect(connectionString);
+mongoose.connect('mongodb://127.0.0.1:27017/socialnetworkapi', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+})
+.then(() => console.log('MongoDB connected'))
+.catch(err => console.error('MongoDB connection error:', err));
 
 module.exports = mongoose.connection;
